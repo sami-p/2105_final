a763df584c54a239fb0001fdd136ab9ef3d15427
sami-p <sami.peterson14@gmail.com>
2021-06-16 10:19:28 -0500

    Create count_cars method, modify cargo hash

--- lib/train.rb
+++ lib/train.rb
@@ -6,6 +6,10 @@ class Train
   def initialize(train_info)
     @name = train_info[:name]
     @type = train_info[:type]
-    @cargo = {}
+    @cargo = Hash.new(0)
+  end
+
+  def count_cars(car)
+    @cargo[car]
   end
 end
