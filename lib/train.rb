c2efd4c913dc1f78e3408c074969c120be1491db
sami-p <sami.peterson14@gmail.com>
2021-06-16 10:16:27 -0500

    Initialize Train with empty cargo hash

--- lib/train.rb
+++ lib/train.rb
@@ -1,9 +1,11 @@
 class Train
   attr_reader :name,
-              :type
-              
+              :type,
+              :cargo
+
   def initialize(train_info)
     @name = train_info[:name]
     @type = train_info[:type]
+    @cargo = {}
   end
 end
