962010c971e3b7610907ec0c9bc333298dedd60d
sami-p <sami.peterson14@gmail.com>
2021-06-16 10:38:17 -0500

    Add empty trains array to initialization

--- lib/train_yard.rb
+++ lib/train_yard.rb
@@ -1,7 +1,9 @@
 class TrainYard
-  attr_reader :location
-  
+  attr_reader :location,
+              :trains
+
   def initialize(train_yard_info)
     @location = train_yard_info[:location]
+    @trains = []
   end
 end
