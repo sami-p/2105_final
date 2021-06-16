903d1595baae2ca1bf9eae1f1ffd9be3082f3f57
sami-p <sami.peterson14@gmail.com>
2021-06-16 10:09:05 -0500

    Test multiple instances of Car

--- spec/car_spec.rb
+++ spec/car_spec.rb
@@ -3,6 +3,7 @@ require './lib/car'
 RSpec.describe Car do
   before :all do
     @car1 = Car.new({type: 'Mail', weight: 5})
+    @car2 = Car.new({type: 'Passengers', weight: 1})
   end
   describe 'instantiation' do
     it 'exists' do
@@ -12,6 +13,10 @@ RSpec.describe Car do
     it 'has attributes' do
       expect(@car1.type).to eq('Mail')
       expect(@car1.weight).to eq(5)
+    end
+
+    it 'can create another instance of Car' do
+      expect(@car2).to be_an_instance_of(Car)
     end
   end
 end
