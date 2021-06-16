354ba993082d21d2da14f3ad4d0f89b8fb41d205
sami-p <sami.peterson14@gmail.com>
2021-06-16 10:34:01 -0500

    Create TrainYard test / test instantiation of class

---
+++ spec/train_yard_spec.rb
@@ -0,0 +1,36 @@
+require './lib/car'
+require './lib/train'
+require './lib/train_yard'
+
+RSpec.describe TrainYard do
+  before :all do
+    @train_yard = TrainYard.new({location: 'Brighton'})
+
+    @car1 = Car.new({type: 'Mail', weight: 5})
+    @car2 = Car.new({type: 'Passengers', weight: 1})
+    @car3 = Car.new({type: 'Cattle', weight: 3})
+    @car4 = Car.new({type: 'Dining', weight: 4})
+
+    @train1 = Train.new({name: 'Thomas', type: 'Tank'})
+    @train2 = Train.new({name: 'Emily', type: 'Tender'})
+    @train3 = Train.new({name: 'Nia', type: 'Tank'})
+    @train4 = Train.new({name: 'Gordon', type: 'Express'})
+
+    @train1.add_cars(@car1, 5)
+    @train1.add_cars(@car2, 2)
+
+    @train2.add_cars(@car1, 3)
+    @train2.add_cars(@car4, 4)
+
+    @train3.add_cars(@car1, 1)
+
+    @train4.add_cars(@car4, 5)
+    @train4.add_cars(@car3, 10)
+  end
+
+  describe 'instantiation' do
+    it 'exists' do
+      expect(@train_yard).to be_an_instance_of(TrainYard)
+    end 
+  end
+end
